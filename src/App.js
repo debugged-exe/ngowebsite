ae185ec277cccf082743685ec3fcb6197b8bce39
Sarah-2000 <sjdkazi@gmail.com>
2021-07-06 17:06:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import HomeBackground from './components/HomeBackground/HomeBackground';
+
+function App() {
+  return (
+    <div>
+     <HomeBackground/> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HomeBackground/HomeBackground.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+function HomeBackground(){
+    return(
+        <h1> Hello User ! </h1>
+    )
+}
+
+export default HomeBackground;
