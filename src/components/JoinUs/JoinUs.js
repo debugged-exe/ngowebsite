2f827aa0ff9e2891e0c48317c694c929e023c775
Sarah-2000 <sjdkazi@gmail.com>
2021-08-01 01:17:21 +0530

    added JOIN US page

--- src/components/JoinUs/JoinUs.js
+++ src/components/JoinUs/JoinUs.js
@@ -7,20 +7,20 @@ function Contact() {
         <div className='margin'></div>
         <section className='parallax' >
             <div className='parallax-inner flex flex-column justify-center items-center'>
-                <h1 >Contact Us</h1>
-                <div className='mv2'>
-                    <p style={{left:'0'}}>Name</p> <br/><br/>
-                    <input className='bg-none br2 inner1' placeholder='Enter Name'/>
-                </div >
-                <div className='mv2'>
-                    <p style={{left:'0'}}>Email</p><br/><br/>
-                    <input className='bg-none br2 inner1' placeholder='Enter Email'/>
+                <h1 >JOIN US</h1>
+                <div style={{textAlign:'center'}}>
+                    <h3>Want to catch up on whats been happening lately? <br/><br/>Take a look at the latest news of Lambda<br/><br/></h3>
                 </div>
-                <div className='mv2'>
-                    <p style={{left:'0'}}>Message</p><br/><br/>
-                    <input className='bg-none br2 inner1' placeholder='Enter Message'/>
-                </div>
-                <a  className="mt4 br2 grow f4 link ph3 pv2 mb2 dib white bg-dark-green but1" href="#0">Send Query</a>
+                <div style={{display:'flex',flexDirection:'column'}}>
+                 <div style={{display:'flex'}}>
+                    <input className='bg-none input-reset pa2 mb3 gold mr4 db w-100 #e88f0a' placeholder='Your Name *'/>
+                    <input className='bg-none input-reset pa2 mb3 gold db w-100' placeholder='Your Email *'/>
+                 </div >
+                 <input className='bg-none input-reset pa2 mb3 gold db w-95' placeholder='Your Subject'/>
+                 <textarea style={{height:'200px'}} className='bg-none input-reset gold w-100 pa2 mb2 dib' placeholder='Your message'/>
+                 
+                 </div>
+                 <a className="f6 link dim ph3 pv2 mb2 dib white bg-gold" href="#0">Send Query</a>
             </div>
         </section>
         <div className='margin'></div>
