1603fc059ef535275307d61df7897a25107ce56e
sayali-s <shindesayali85@gmail.com>
2021-08-01 01:26:20 +0530

    About Page

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,21 +1,34 @@
-import React from 'react'
-import './AboutUs.css'
-import image1 from './img.png'
+import React from 'react';
+import './AboutUs.css';
+import Poor from './poorkids1.png';
+import 'tachyons'
 
-function AboutUs(){
-    return(
-          <div className="container-div">
-            <div>
-              <img src={image1} alt="image"/>
-            </div>
-            <div>
-               <h1 style={{textAlign:"left"}}>About Us</h1>
-               <p>Rohit rose nursery established in the year 1990 located on bhigwan- walchandnagar road in kalas,Tal-Indapur Dist-Pune(Maharashtra).3 km from Pune- Solapur highway. We produce and supply all types of flower plants,fruit plants, decorative plants and shade plants. We supply plants and also give you information on how to go about taking care and growing them.
-                We also specialise in the quality plant of rose, pomegranate,fig and grapes root variety.
-               </p>
-            </div>
-          </div>
-    )
+function AboutUs() {
+  return (
+    <div>
+
+
+      <div className="tc">
+        <h2 className="f2 pt4">ABOUT CHARITY</h2>
+        <div className="line tc bg-yelloe"></div>
+        <h3 className="tc mt2 about-line">We're an international organisation working with people in 50 countries for a world free from poverty and injustice.</h3>
+      </div>
+
+      <div className="grid-container ma3">
+        <img src={Poor} height="550px" className="ml5" />
+        <div>
+          <h5 className="f5 mt5">We’re an international development organisation with our head office based in <b>Greece</b>. We also have offices right across Asia, the Americas and Europe. We <b>believe</b> the people whose lives our work affects should decide how we’re run.
+            <br /><br />
+            And that’s what makes us different. We help people use their own power to fight poverty and injustice.
+            <br /><br />
+            You can help us make a fairer, better world – wherever you are. Your voice can persuade world leaders to change policies.
+          </h5>
+          <a className="f4 link grow ph3 br2 pv2 mb2 dib bg-yelloe white" href="#0"><i><b>JOIN OUR CAUSE</b></i></a>
+        </div>
+      </div>
+
+    </div >
+  );
 }
 
 export default AboutUs;
