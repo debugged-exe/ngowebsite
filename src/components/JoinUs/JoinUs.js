2eab61a81ca77f9d13e27f25767e41b3225f011b
Sarah-2000 <sjdkazi@gmail.com>
2021-08-04 01:30:04 +0530

    added Latest Events page,added icons to Join Us form,updated it

--- src/components/JoinUs/JoinUs.js
+++ src/components/JoinUs/JoinUs.js
@@ -1,5 +1,8 @@
 import React from "react";
 import 'tachyons';
+import { MdEmail } from 'react-icons/md'
+import { FaPen } from 'react-icons/fa'
+import { BsFillPersonFill,BsFileEarmarkSpreadsheet } from 'react-icons/bs'
 import './JoinUs.css'
 function Contact() {
     return (
@@ -8,19 +11,32 @@ function Contact() {
         <section className='parallax' >
             <div className='parallax-inner flex flex-column justify-center items-center'>
                 <h1 >JOIN US</h1>
+                <div className="line tc bg-white"></div>
                 <div style={{textAlign:'center'}}>
                     <h3>Want to catch up on whats been happening lately? <br/><br/>Take a look at the latest news of Lambda<br/><br/></h3>
                 </div>
                 <div style={{display:'flex',flexDirection:'column'}}>
                  <div style={{display:'flex'}}>
-                    <input className='bg-none input-reset pa2 mb3 gold mr4 db w-100 #e88f0a' placeholder='Your Name *'/>
-                    <input className='bg-none input-reset pa2 mb3 gold db w-100' placeholder='Your Email *'/>
+
+                    <BsFillPersonFill size="3rem" color="#F1C34F" style={{marginRight:'-2rem'}} />
+                    <input className='bg-none pa2 mb3 yelloe mr4 db w-100 ' placeholder='Your Name *'/>
+                    
+                    <MdEmail size="3rem" color="#F1C34F" style={{marginRight:'-2rem'}} />
+                    <input className='bg-none pa2 mb3 yelloe db w-100' type='email' placeholder='Your Email *'/>
+                   
                  </div >
-                 <input className='bg-none input-reset pa2 mb3 gold db w-95' placeholder='Your Subject'/>
-                 <textarea style={{height:'200px'}} className='bg-none input-reset gold w-100 pa2 mb2 dib' placeholder='Your message'/>
+                 <br/>
+                
+                 <BsFileEarmarkSpreadsheet size="1.2rem" color="#F1C34F" style={{marginLeft:'1rem',marginBottom:'-2rem'}} />
+                 <input className='bg-none pa2 mb4 yelloe db w-95' placeholder='Your Subject'/>
+                 
+
+                 <FaPen size="1.2rem" color="#F1C34F" style={{marginLeft:'1rem',marginBottom:'-2rem'}} />
+                 <textarea style={{height:'200px'}} className='bg-none yelloe w-100 pa2 mb4 dib' placeholder='Your message'/>
+                 
                  
                  </div>
-                 <a className="f6 link dim ph3 pv2 mb2 dib white bg-gold" href="#0">Send Query</a>
+                 <a className="f6 link dim ph3 pv2 mb2 dib white bg-yelloe" href="#0">Send Email</a>
             </div>
         </section>
         <div className='margin'></div>
--- src/components/Testimonials/testimonials.js
+++ src/components/Testimonials/testimonials.js
@@ -8,24 +8,43 @@ function testimonials(){
             <h1 style={{position:'relative',top:'25%',paddingTop:'10%',paddingBottom:'10px'}}> <b>Latest Events</b></h1>
             <div className='contain'>
                 <div className='category' style={{backgroundImage:'url('+poor1+')',backgroundSize:'cover',position:'relative'}}>
-                    <div className='inner'>Hello World</div>
+                <div className='inner'>
+                     Hello World
+                    <hr style={{bottom:'0',width:'80%'}}/>
+                </div>
+                    <div className='hidden'> New component</div>
                 </div>
                 <div className='category' style={{backgroundImage:'url('+poor1+')',backgroundSize:'cover',position:'relative'}}>
-                    <div className='inner'>Hello World</div>
+                    <div className='inner'>Hello World
+                    <hr style={{bottom:'0',width:'80%'}}/>
+                    </div>
+                    <div className='hidden'>New component</div>
                 </div>
                 <div className='category' style={{backgroundImage:'url('+poor1+')',backgroundSize:'cover',position:'relative'}}>
-                    <div className='inner'>Hello World</div>
+                    <div className='inner'>Hello World
+                    <hr style={{bottom:'0',width:'80%'}}/>
+                    </div>
+                    <div className='hidden'>New component</div>
                 </div>
             </div>
             <div className='contain' style={{paddingBottom:'10%'}}>
                 <div className='category' style={{backgroundImage:'url('+poor1+')',backgroundSize:'cover',position:'relative'}}>
-                    <div className='inner'>Hello World</div>
+                    <div className='inner'>Hello World
+                    <hr style={{bottom:'0',width:'80%'}}/>
+                    </div>
+                    <div className='hidden'>New component</div>
                 </div>
                 <div className='category' style={{backgroundImage:'url('+poor1+')',backgroundSize:'cover',position:'relative'}}>
-                    <div className='inner'>Hello World</div>
+                    <div className='inner'>Hello World
+                    <hr style={{bottom:'0',width:'80%'}}/>
+                    </div>
+                    <div className='hidden'>New component</div>
                 </div>
                 <div className='category' style={{backgroundImage:'url('+poor1+')',backgroundSize:'cover',position:'relative'}}>
-                    <div className='inner'>Hello World</div>
+                    <div className='inner'>Hello World
+                    <hr style={{bottom:'0',width:'80%'}}/>
+                    </div>
+                    <div className='hidden'>New component</div>
                 </div>
             </div>
         </div>
