9ec46784a97b96bade75d0546da587f1727cb728
Sarah-2000 <sjdkazi@gmail.com>
2021-08-11 23:42:53 +0530

    navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react';
 import './navbar.css';
 import logo from './charity-white.png';
 import { HashLink as Link } from 'react-router-hash-link';
+import {GiHamburgerMenu} from 'react-icons/gi';
 
 function Navbar() {
-
+    const [navbar, setNavbar] = useState('');
     const [show, handleShow] = useState(false);
     useEffect(() => {
         window.addEventListener("scroll", () => {
@@ -18,7 +19,7 @@ function Navbar() {
     }, []);
 
     return (
-        <div className={`nav ${show && "nav_back"}`}>
+        <div className={`nav ${show && "nav_back"} navbar ${navbar==='block'?'vertical':''} `}>
             <div className="db dt-l w-100 border-box pa3 ph5-l">
                 <Link className="db dtc-l v-mid mid-gray link1 dim w-100 w-25-l tl-l mb2 mb0-l" title="Home" to='/'>
                     <div className="flex justify-center items-center">
@@ -26,7 +27,9 @@ function Navbar() {
                         <span className='b f4 ml3'><i>Charity</i></span><br />
                     </div>
                 </Link>
-                <div className="db dtc-l v-mid w-100 w-75-l tr-l">
+
+                <div className='hide-navlist' style={{display:`${navbar}`}}>
+                
                     <Link className="link1 dim dark-gray f6 f5-l dib mr3 mr4-l" to='/HomeBackground'>HOME</Link>
                     <Link className="link1 dim dark-gray f6 f5-l dib mr3 mr4-l" to='/AboutUs'>ABOUT US</Link>
                     <Link className="link1 dim dark-gray f6 f5-l dib mr3 mr4-l" to='/OurGoals'>OUR GOALS</Link>
@@ -34,9 +37,12 @@ function Navbar() {
                     <Link className="link1 dim dark-gray f6 f5-l dib mr3 mr4-l" to="/News">NEWS</Link>
                     <Link className="link1 dim dark-gray f6 f5-l dib mr3 mr4-l" to="/JoinUs">JOIN US</Link>
                     <Link className="f6 link br2 dim ph3 pv2 mb2 dib white bg-yelloe" href="#0"><b>DONATE</b></Link>
-                </div>
+                    </div>
+                
+                <GiHamburgerMenu style={{display:`${navbar==='block'?'none':''}`}} onClick={()=>setNavbar('block')} className='hide-ham show-ham' size='1.8rem'/>
             </div>
             <div className="line bg-white" style={{ width: '100%', height: '0.5px', opacity: '0.2' }}></div>
+            
         </div>
     );
 }
