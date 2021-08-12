9c102d315756d30bda81678e690d4c2900a6e162
sayali-s <shindesayali85@gmail.com>
2021-08-12 12:42:39 +0530

    deploy

--- src/components/HomeBackground/HomeBackground.js
+++ src/components/HomeBackground/HomeBackground.js
@@ -11,7 +11,7 @@ class HomeBackground extends Component {
   render() {
     return (
       <div className="home-div">
-        <Carousel itemsToShow={1} style={{ position: 'relative', right: '-10px', left: '-10px', width: '102%' }} showArrows={false} enableAutoPlay={true} interval={3000}>
+        <Carousel itemsToShow={1} style={{ position: 'relative', right: '-15px', left: '-15px', width: '102%' }} showArrows={false} enableAutoPlay={true} interval={3000}>
 
           <div className='slide bg-image flex flex-column justify-center items-center' style={{ backgroundImage: 'url(' + homebg1 + ')', backgroundSize: 'cover', width: '100vw' }}>
             <p className="sty f4">THE WAR AGAINST <b>HUNGER</b><br />IS THE WAR OF <b>LIBERATION</b></p>
